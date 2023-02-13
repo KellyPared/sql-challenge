60dde3cd4bb0dd7205513478fbec3a8866c78eed
Kelly Schuster-Paredes <40581033+KellyPared@users.noreply.github.com>
2023-02-13 17:14:59 -0500

    numbered

--- EmployeeSQL/manger_info.sql
+++
@@ -1,8 +0,0 @@
--- manager of each department 
--- along with their department number, 
--- department name, employee number, last name, and first name
-
-
-
-	(SELECT dept_no, emp_no
-	FROM "Department_Manager");
