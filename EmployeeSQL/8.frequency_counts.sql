02693bc887dbb7322c1056aa1cbe77c5aadb9930
Kelly Schuster-Paredes <40581033+KellyPared@users.noreply.github.com>
2023-02-15 09:37:50 -0500

    views

--- EmployeeSQL/8.frequency_counts.sql
+++ EmployeeSQL/8.frequency_counts.sql
@@ -23,4 +23,9 @@ GROUP BY E.last_name
 ORDER BY (E.last_name) DESC,
 --(WHERE SELECT TRANSLATE(E.last_name, '''d'Astous''', 'dAstous'),
 	
-
+CREATE VIEW frequency_last_names AS
+SELECT E.last_name,
+COUNT(DISTINCT E.last_name)
+FROM "Employees" AS E
+GROUP BY E.last_name 
+ORDER BY left(E.last_name,1) DESC;
